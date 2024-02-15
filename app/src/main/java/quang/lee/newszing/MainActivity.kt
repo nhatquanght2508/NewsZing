235fcac3fb53c6b322ca9bc6fa9ba0ed70b3cef4
nhatquang <nhatquanght2508>
2024-02-15 11:25:17 +0700

    Implement navgraph

--- app/src/main/java/quang/lee/newszing/MainActivity.kt
+++ app/src/main/java/quang/lee/newszing/MainActivity.kt
@@ -1,45 +1,36 @@
 package quang.lee.newszing
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import quang.lee.newszing.domain.usecases.AppEntryUseCases
-import quang.lee.newszing.presentation.onboarding.OnBoardingScreen
-import quang.lee.newszing.presentation.onboarding.OnBoardingViewModel
+import quang.lee.newszing.presentation.nvgraph.NavGraph
 import quang.lee.newszing.ui.theme.NewsZingTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-        lifecycleScope.launch {
-            appEntryUseCases.readAppEntry().collect {
-                Log.d("Test", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition
             }
         }
         setContent {
             NewsZingTheme {
-                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background))
-                val viewModel : OnBoardingViewModel = hiltViewModel()
-                OnBoardingScreen(event = {
-                    viewModel.onEvent(it)
-                })
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
+                }
             }
         }
     }
---
+++ app/src/main/java/quang/lee/newszing/MainViewModel.kt
@@ -0,0 +1,38 @@
+package quang.lee.newszing
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import quang.lee.newszing.domain.usecases.AppEntryUseCases
+import quang.lee.newszing.presentation.nvgraph.Route
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val appEntryUseCases: AppEntryUseCases
+) : ViewModel() {
+
+    var splashCondition by mutableStateOf(true)
+    private set
+
+    var startDestination by mutableStateOf(Route.AppStartNavigation.route)
+    private set
+
+    init {
+        appEntryUseCases.readAppEntry().onEach { shouldStartFromDestination ->
+            if (shouldStartFromDestination) {
+                startDestination = Route.NewsNavigation.route
+            } else {
+                startDestination = Route.AppStartNavigation.route
+            }
+            delay(300)
+            splashCondition = false
+        }.launchIn(viewModelScope)
+    }
+}
